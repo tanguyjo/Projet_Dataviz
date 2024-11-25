f40dae9d64687c26d1531721083b69ca9b763657
Evan <evanaguttesrivelon@gmail.com>
2024-11-25 15:56:37 +0100

    rangement du code

--- dataviz.js
+++ dataviz.js
@@ -1,49 +1,37 @@
 const div = document.querySelector('.feuille');
 const h1 = document.querySelector('h1')
+const XMAX = 1075
+const XMIN = 400
+const YMAX = 450
+const YMIN = 0
 
-
-
-
- let timer;                                                                                                         
- let date = new Date()                                                          
-function startTimer (){
-                                                                  
-   timer = setInterval(()=>{  
-  date = new Date()
- let temps = date.getHours() + ":" +(date.getMinutes() < 10 ? '0' : '') + date.getMinutes() + ":" +(date.getSeconds() < 10 ? '0' : '') + date.getSeconds();      
- (date.getMinutes() < 10 ? '0' : '') + date.getMinutes()
- ;                                                     
-	h1.innerHTML =temps;
-   }, 1000);}                                                                             
- 
+let timer;  
+let date = new Date()
 let heur = date.getHours()
-startTimer()
+let randomx = 0;
+let randomy = 0;
+let storagefeuille = [ ];
+let compteur = 0;
 
-if(heur>16){
-
-	 div.style.filter = "grayscale(0.8)"
+function startTimer (){
+	timer = setInterval(()=>{  
+	date = new Date()
+	let temps = date.getHours() + ":" +(date.getMinutes() < 10 ? '0' : '') + date.getMinutes() + ":" +(date.getSeconds() < 10 ? '0' : '') + date.getSeconds();                                                
+	h1.innerHTML =temps;
+   }, 1000);
 }
 
-   if(sessionStorage.getItem("key")){
-
-	 let storagefeuille = JSON.parse(sessionStorage.getItem("key"))
-	 console.log(storagefeuille)
-	 
-
-	
-	
-	 
-   }
-   else {
-	
-	addfeuilles()
-   }
-
+function getRandomx() {
+	randomx = Math.random() * (XMAX - XMIN) + XMIN;
+	return randomx
+}
+function getRandomy() {
+	randomy = Math.random() * (YMAX - YMIN) + YMIN;
+	return randomy
+}
 
 function adduneFeuille(x,y)
 {
-	// let x = 450;
-	// let y = 300;
 	const img = document.createElement('img')
 	img.style.position = "absolute"
 	img.style.left = x + 'px'
@@ -51,46 +39,38 @@ function adduneFeuille(x,y)
 	img.setAttribute("src", "leaf1.png")
 	img.style.height = "75px";
 	div.appendChild(img)
-
-
 }
 
-let storagefeuille = [ ];
-let compteur =0;
 function addfeuilles(){
-let addfeuille = setInterval (()=> {
-	getRandomx()
-	getRandomy()
-	storagefeuille.push({x : randomx, y : randomy})
-	sessionStorage.setItem("key",JSON.stringify(storagefeuille));
-	adduneFeuille(randomx,randomy);
-	compteur++;
-	console.log(compteur)
-	if (compteur == 50){
-		clearInterval(addfeuille)
-		console.log(storagefeuille)
-	}
-   
-},100)}
-
-
-
-
- 
-var randomx = 0;
-var randomy = 0;
-
-function getRandomx() {
+	let addfeuille = setInterval (()=> {
+		getRandomx()
+		getRandomy()
+		storagefeuille.push({x : randomx, y : randomy})
+		sessionStorage.setItem("key",JSON.stringify(storagefeuille));
+		adduneFeuille(randomx,randomy);
+		compteur++;
+		console.log(compteur)
+		if (compteur == 50){
+			clearInterval(addfeuille)
+			console.log(storagefeuille)
+		}
 	
-	randomx = Math.random() * (1075 - 400) + 400;
-	return randomx
-  }
-  function getRandomy() {
-	
-	randomy = Math.random() * (450 - 0) + 0;
-	return randomy
-  }
+	}, 100)
+}
+
  // x min = 400  et 1075
  // y min = 0  et max  450
 
-console.log(randomx)
+if(heur>16){
+	div.style.filter = "grayscale(0.8)"
+}
+
+if(sessionStorage.getItem("key")) {
+	let storagefeuille = JSON.parse(sessionStorage.getItem("key"))
+	console.log(storagefeuille)
+}
+else {
+	addfeuilles()
+}
+
+startTimer()
