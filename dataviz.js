5df04367ddf4e06a48957403758e15ce3acf9c98
Evan <evanaguttesrivelon@gmail.com>
2024-11-25 12:29:20 +0100

    img postion change

--- dataviz.js
+++ dataviz.js
@@ -2,7 +2,12 @@ const div = document.querySelector('.feuille');
 
 function addFeuille()
 {
+	let x = 450;
+	let y = 300;
 	const img = document.createElement('img')
+	img.style.position = "absolute"
+	img.style.left = x + 'px'
+	img.style.top = y + 'px'
 	img.setAttribute("src", "feuille.jpg")
 	img.style.height = "75px";
 	div.appendChild(img)
